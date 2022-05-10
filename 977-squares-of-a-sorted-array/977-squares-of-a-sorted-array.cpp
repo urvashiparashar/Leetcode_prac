9210c801aa051e443aa4d8a2aa388f105d5e40b0
Urvashi Parashar <urvashiparashar2001@gmail.com>
2022-05-10 21:30:57 +0530

    Time: 39 ms (68.80%), Space: 26 MB (55.38%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
         vector<int>ans(nums.size());
-        int l=0,r=nums.size()-1,k=nums.size()-1;
+        int l=0,r=nums.size()-1,k=nums.size()-1;//k is piche se kyunki last ele sorted hota ja rha h at each iteration
         while(l<=r)
         {
             if(abs(nums[r])>abs(nums[l]))
             {
-                ans[k]=nums[r]*nums[r];
-                r--;
-                k--;
+                ans[k]=nums[r]*nums[r]; //setting the kth ele at right posn
+                r--; //r is bigger 
+                k--; //k is bigger
             }
             else
             {
-                ans[k]=nums[l]*nums[l];
-                l++;
-                k--;
+                ans[k]=nums[l]*nums[l];//setting the kth ele at right posn
+                l++; //l is smaller
+                k--; //k is bigger
             }
         }
         return ans;
