bfe5483b4d9a73519e91501f00a3ebdbc8163489
Urvashi Parashar <urvashiparashar2001@gmail.com>
2022-05-29 00:46:24 +0530

    Time: 136 ms (38.65%), Space: 46.5 MB (92.00%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(),nums.end());
-        int t=0;
+        int n = nums.size(); 
+        sort(nums.begin(),nums.end()); //sorting array in ascending order
+        int t=0; //taking a var and initilaisng it as 0
         for(int j=0;j<n-1;j++)
         {
             if(nums[j]==nums[j+1])
             {
-                t=1;
+                t=1;//changing value of t to 1 as soon as duplicate is found i.e consecutive two ele are same
             }
         }
         if(t==1)
