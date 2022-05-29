76418f200a3178d66935cb1497e53225c7c50a7c
Urvashi Parashar <urvashiparashar2001@gmail.com>
2022-05-29 22:22:12 +0530

    Time: 3 ms (41.76%), Space: 6.4 MB (5.23%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -3,7 +3,7 @@ public:
     int maxDepth(string s) {
         int maxi=0,cnt=0;
         stack<char>vps;
-        //taking stack and pushing the opening brackets
+        
         for(int i=0;i<s.length();i++)
         {
             
@@ -12,13 +12,14 @@ public:
             {
                 //push in stack
                 vps.push(s[i]);
+                maxi=maxi>vps.size()?maxi:vps.size();
             }
             else 
             {
                 if(s[i]==')') //when closing bracket is found compare with max and pop the corressponding opening bracket
                     
                 {
-                    maxi=max(maxi,(int)vps.size()); //.size() is of size_t so explicit casting is a must
+                    
                     vps.pop();
                 }
             }
