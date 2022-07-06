3dc81a0eb7fca67cec88b6e3ae2fcf2cac95add9
Urvashi Parashar <urvashiparashar2001@gmail.com>
2022-07-06 19:20:52 +0530

    Time: 131 ms (79.15%), Space: 67.7 MB (53.48%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        //kadane's algo
-        int sum=0;
+        //kadane's algo: to get maximum sum subarray
         int maxi=nums[0];
+        int sum=0;
+        //traversing the array
         for(int i=0;i<nums.size();i++)
         {
-           //traversing array
-            //updating sum
-            sum+=nums[i];
-           //update max
-            maxi=max(maxi,sum);
+            sum+=nums[i]; //taking the every element of nums vector
+            if(sum>maxi)
+            {
+                maxi=sum;
+            }
             if(sum<0)
             {
+                //if sum is negative then take sum as 0
                 sum=0;
-                
             }
         }
         return maxi;
